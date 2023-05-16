133c83d722087b502d983d5f6b5b2ed7aa0b7fa1
Pedro <a.poliveira28792@aeestarreja.pt>
2023-05-16 14:53:21 +0100

    melhoria na cracao do quizz

--- Testes/menuFooter/testeMenu.php
+++ Testes/menuFooter/testeMenu.php
@@ -1,27 +1,8 @@
+<link href="testeMenu.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-<link rel="stylesheet" type="text/css" href="menuFooter/testeMenu.css">
-
-
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
- <div class="container-fluid">
-  <a class="navbar-brand" href="#">Brand</a>
-  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main_nav"  aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="main_nav">
-	<ul class="navbar-nav">
-		<li class="nav-item active"> <a class="nav-link" href="#">Home </a> </li>
-		<li class="nav-item"><a class="nav-link" href="#"> About </a></li>
-		<li class="nav-item"><a class="nav-link" href="#"> Services </a></li>
-		<li class="nav-item dropdown">
-		   <a class="nav-link  dropdown-toggle" href="#" data-bs-toggle="dropdown">  Hover me  </a>
-		    <ul class="dropdown-menu">
-			  <li><a class="dropdown-item" href="#"> Submenu item 1</a></li>
-			  <li><a class="dropdown-item" href="#"> Submenu item 2 </a></li>
-			  <li><a class="dropdown-item" href="#"> Submenu item 3 </a></li>
-		    </ul>
-		</li>
-	</ul>
-  </div> <!-- navbar-collapse.// -->
- </div> <!-- container-fluid.// -->
-</nav>
+<div class="page-header">
+	<a id="menu-icon" class="menu-icon">
+		<i class="fa fa-bars"></i>
+	</a>
+</div>
