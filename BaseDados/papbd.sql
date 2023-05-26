47c38a9893cf064277d95ae018e43d995a60220d
Pedro <a.poliveira28792@aeestarreja.pt>
2023-05-26 14:58:38 +0100

    atualização da base de dados

--- BaseDados/papbd.sql
+++ BaseDados/papbd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 22-Maio-2023 às 17:52
+-- Tempo de geração: 26-Maio-2023 às 15:57
 -- Versão do servidor: 10.4.21-MariaDB
 -- versão do PHP: 8.0.11
 
@@ -59,23 +59,21 @@ CREATE TABLE `noficacoes` (
 CREATE TABLE `questoes` (
   `Id_questao` int(11) NOT NULL,
   `Id_quizz` int(11) NOT NULL,
-  `nomeQuestao` varchar(20) DEFAULT NULL,
+  `nomeQuestao` varchar(60) DEFAULT '',
   `textoQuestao` varchar(2500) DEFAULT NULL,
-  `imagem` varchar(2500) DEFAULT NULL
+  `imagem` varchar(2500) DEFAULT NULL,
+  `tipoQuestao` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Extraindo dados da tabela `questoes`
 --
 
-INSERT INTO `questoes` (`Id_questao`, `Id_quizz`, `nomeQuestao`, `textoQuestao`, `imagem`) VALUES
-(1, 12, NULL, 'sdgsd', NULL),
-(2, 12, NULL, 'wegweg', NULL),
-(3, 12, NULL, 'wegweg', NULL),
-(4, 12, NULL, 'wegweg', NULL),
-(5, 12, NULL, 'wegweg', NULL),
-(6, 12, NULL, 'wegweg', NULL),
-(7, 12, NULL, 'wegweg', NULL);
+INSERT INTO `questoes` (`Id_questao`, `Id_quizz`, `nomeQuestao`, `textoQuestao`, `imagem`, `tipoQuestao`) VALUES
+(108, 16, 'Pepsi ou coca-cola', 'O que se passa na tua cabeça quando bebes uma coca cola?', '', 'mostrarAcerto'),
+(109, 16, 'Bom dia', 'Bom dia', '', 'escreverResposta'),
+(115, 16, '', 'Qual é o mês do natal?', '/nothingToSee.jpeg', 'mostrarAcerto'),
+(127, 16, '', 'erwaegwEGwe', '/61eb8ee4a33d5d634eec750090b4ffa4.jpg', 'mostrarAcerto');
 
 -- --------------------------------------------------------
 
@@ -97,8 +95,7 @@ CREATE TABLE `quizzes` (
 --
 
 INSERT INTO `quizzes` (`Id_quizz`, `Id_utilizador`, `nomeQuizz`, `categoria`, `escolaridade`, `tema`) VALUES
-(7, 356, NULL, NULL, 'temporario', NULL),
-(12, 351, NULL, NULL, 'temporario', NULL);
+(16, 355, NULL, NULL, 'temporario', NULL);
 
 -- --------------------------------------------------------
 
@@ -123,8 +120,34 @@ CREATE TABLE `respostas` (
   `Id_resposta` int(11) NOT NULL,
   `Id_questao` int(11) NOT NULL,
   `respostaQuizz` varchar(2500) DEFAULT NULL,
-  `valorResposta` int(3) DEFAULT NULL
+  `valorResposta` varchar(10) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `respostas`
+--
+
+INSERT INTO `respostas` (`Id_resposta`, `Id_questao`, `respostaQuizz`, `valorResposta`) VALUES
+(202, 108, 'Busca de memória \n&gt; \nBusca de sabor', 'true'),
+(203, 108, 'Busca de memória \n&lt;\nBusca de sabor', 'false'),
+(204, 108, 'Busca de memória \n=\nBusca de sabor', 'false'),
+(205, 108, 'Só busca de memória<br>', 'false'),
+(206, 109, 'test', 'true'),
+(207, 109, 'test', 'true'),
+(208, 109, 'test', 'true'),
+(209, 109, 'test', 'true'),
+(210, 109, 'test', 'true'),
+(211, 109, 'test', 'true'),
+(212, 109, 'test', 'true'),
+(213, 109, 'test', 'true'),
+(231, 115, 'Janeiro', 'false'),
+(232, 115, 'Fevereiro', 'false'),
+(233, 115, 'Desembro', 'true'),
+(234, 115, 'Novembro', 'false'),
+(277, 127, 'adfgadfgadgdfg', 'false'),
+(278, 127, 'adfgadfgafgg', 'false'),
+(279, 127, 'gWEGWEGWeg', 'false'),
+(280, 127, 'wgarggadgasdf', 'false');
 
 -- --------------------------------------------------------
 
@@ -151,10 +174,7 @@ CREATE TABLE `utilizadores` (
 INSERT INTO `utilizadores` (`Id_utilizador`, `nomeCompleto`, `nomeUnico`, `email`, `password`, `imagemPerfil`, `dataBan`, `pontuacao`, `permissao`) VALUES
 (338, 'Pedro Oliveira', 'Pedr0siris', 'pedro@gmail.com', 'qaws1234', '../BaseDados/Utilizadores/Utilizador_338/61eb8ee4a33d5d634eec750090b4ffa4.jpg', NULL, 700, 'utilizador'),
 (351, 'Robert Stuart', 'stuartL1r0u', 'aaa@aaa.com', 'aaaaaaaa', '../BaseDados/Utilizadores/Utilizador_351/getimg_ai_img-zME5hHcQjXY8zeM7tdS8h.png', NULL, 576, 'utilizador'),
-(354, 'bbb bbb', 'bbb', 'bbb@bbb.com', 'bbbbbbbb', 'img/perfilPadrao.png', NULL, 0, 'utilizador'),
-(355, 'assa asaas', 'aaas', 'aas@asas.com', 'asdasdasda', 'img/perfilPadrao.png', NULL, 0, 'utilizador'),
-(356, 'as as', 'sd', 'sd@sd.com', 'sdsdsdsdsd', 'img/perfilPadrao.png', NULL, 0, 'utilizador'),
-(357, 'asd asd', 'asd', 'asd@asd.com', 'asdasdasd', 'img/perfilPadrao.png', NULL, 0, 'utilizador');
+(355, 'assa asaas', 'aaas', 'aas@asas.com', 'asdasdasda', '../BaseDados/Utilizadores/Utilizador_355/getimg_ai_img-zME5hHcQjXY8zeM7tdS8h.png', NULL, 0, 'utilizador');
 
 --
 -- Índices para tabelas despejadas
@@ -230,13 +250,13 @@ ALTER TABLE `noficacoes`
 -- AUTO_INCREMENT de tabela `questoes`
 --
 ALTER TABLE `questoes`
-  MODIFY `Id_questao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `Id_questao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=132;
 
 --
 -- AUTO_INCREMENT de tabela `quizzes`
 --
 ALTER TABLE `quizzes`
-  MODIFY `Id_quizz` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `Id_quizz` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de tabela `quizzes_respondidos`
@@ -248,13 +268,13 @@ ALTER TABLE `quizzes_respondidos`
 -- AUTO_INCREMENT de tabela `respostas`
 --
 ALTER TABLE `respostas`
-  MODIFY `Id_resposta` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id_resposta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=297;
 
 --
 -- AUTO_INCREMENT de tabela `utilizadores`
 --
 ALTER TABLE `utilizadores`
-  MODIFY `Id_utilizador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=358;
+  MODIFY `Id_utilizador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=362;
 
 --
 -- Restrições para despejos de tabelas
